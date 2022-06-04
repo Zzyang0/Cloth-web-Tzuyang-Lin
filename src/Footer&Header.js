56d7c7f961d74c61a9e271ca4b1acd87a1619f2b
Jiali Liu <Jiali123@uw.edu>
2022-06-03 18:28:38 -0700

    login

--- src/Footer&Header.js
+++ src/Footer&Header.js
@@ -1,125 +1,143 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import the font-icon style
-import {faCaretDown} from '@fortawesome/free-solid-svg-icons';
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
-export function Nav() {
 
-const hamburgerToggle = () => {
-  const hamburger = document.querySelector('.hamburger');
-  const navlink = document.querySelector('.navlink');
-  hamburger.classList.toggle('active');
-  navlink.classList.toggle('active');
-}
+import { getAuth, signOut } from 'firebase/auth';
 
-const navlRemove = () => {
-  const hamburger = document.querySelector('.hamburger');
-  const navlink = document.querySelector('.navlink');
-  hamburger.classList.remove('active');
-  navlink.classList.remove('active');
-}
+export function Nav(props) {
+  const currentUser = props.currentUser.userId;
 
-  return(
+  const hamburgerToggle = () => {
+    const hamburger = document.querySelector('.hamburger');
+    const navlink = document.querySelector('.navlink');
+    hamburger.classList.toggle('active');
+    navlink.classList.toggle('active');
+  }
+
+  const navlRemove = () => {
+    const hamburger = document.querySelector('.hamburger');
+    const navlink = document.querySelector('.navlink');
+    hamburger.classList.remove('active');
+    navlink.classList.remove('active');
+  }
+
+
+  const handleSignOut = (event) => {
+    console.log("signing out");
+    signOut(getAuth());
+  }
+
+  return (
     <section className="navbar">
-    <nav>
-      <header>
-        <h1><Link to='/' className='logo' onClick={navlRemove}>Dvogue</Link></h1>
+      <nav>
+        <header>
+          <h1><Link to='/' className='logo' onClick={navlRemove}>Dvogue</Link></h1>
         </header>
         <ul className='navlink' id='navlink'>
           <li>
-        <Link to='/closet' className='navl' onClick={navlRemove}>My Closet</Link>
+            <Link to='/closet' className='navl' onClick={navlRemove}>My Closet</Link>
           </li>
           <li>
-        <div className="nav-dropdown">
-        <button className="dropbutton">Cloth Generator 
-        <FontAwesomeIcon icon={faCaretDown} />
-        </button>
-        <div className="nav-dropdown-content">
-          <Link to='outfitgenerator' className='navl' onClick={navlRemove}>Outfit Generator</Link>
-          <Link to='itemgenerator' className='navl' onClick={navlRemove}>Item Generator</Link>
-        </div>
-      </div>
-      </li>
-      <li><Link to='/quiz' className='navl' onClick={navlRemove}>Style Quiz</Link></li>
-      <li><Link to='/about' className='navl' onClick={navlRemove}>About Us</Link></li>
-      <li><a href="#" className='navl'><FontAwesomeIcon icon={faUser} />Login</a></li>
+            <div className="nav-dropdown">
+              <button className="dropbutton">Cloth Generator
+                <FontAwesomeIcon icon={faCaretDown} />
+              </button>
+              <div className="nav-dropdown-content">
+                <Link to='outfitgenerator' className='navl' onClick={navlRemove}>Outfit Generator</Link>
+                <Link to='itemgenerator' className='navl' onClick={navlRemove}>Item Generator</Link>
+              </div>
+            </div>
+          </li>
+          <li><Link to='/quiz' className='navl' onClick={navlRemove}>Style Quiz</Link></li>
+          <li><Link to='/about' className='navl' onClick={navlRemove}>About Us</Link></li>
+          {/* <li><Link to='/signin'><FontAwesomeIcon icon={faUser} />Login</Link></li>
+          <li><button className='navl' onClick={handleSignOut}>Sign Out</button></li> */}
+          {!currentUser &&
+            <li><Link to='/signin'><FontAwesomeIcon icon={faUser} />Login</Link></li>
+          }
+          {currentUser && <>
+            <li><Link to='/signin' onClick={handleSignOut}>Sign Out</Link></li>
+          </>
+          }
 
-      </ul>
-      <div className="hamburger" onClick={hamburgerToggle}>
-        <span className="bar"></span>
-        <span className="bar"></span>
-        <span className="bar"></span>
-      </div>
-    </nav>
+        </ul>
+        <div className="hamburger" onClick={hamburgerToggle}>
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </div>
+      </nav>
     </section>
-    )
+  )
 
 }
 
 export function Footer() {
 
-    return(
-        <footer className="footer">
-        <ul className="footer1">
-    
-          <li className="footeritem">
-          <h2><Link to='/' className="title">Home</Link></h2>
-          </li>
-      
-          <li className="footeritem">
-          <h2><Link to='outfitgenerator' className="title">Cloth generator</Link></h2>
-            <ul className="footerul">
-              <li>
-                <Link to='outfitgenerator'>Generate full outfit</Link>
-              </li>
-              <li>
-              <Link to='itemgenerator'>Generate items</Link>
-              </li>
-            </ul>
-          </li>
-      
-          <li className="footeritem">
-            <h2><Link to='/quiz'className="title">Style Quiz</Link></h2>
-          </li>
-      
-          <li className="footeritem">
-            <h2><Link to='/about' className="title">About</Link></h2>
-          </li>
-      
-          <li className="footeritem">
-            <h2 className="title">Social</h2>
-      
-            <ul className="footerul">
-              <li>
-                <a href="https://www.instagram.com/">Our instagram</a>
-              </li>
-      
-              <li>
-                <a href="https://www.facebook.com/">Our Facebook</a>
-              </li>
-      
-              <li>
-                <a href="https://twitter.com/?lang=en">Our twitter</a>
-              </li>
-            </ul>
-          </li>
-        </ul>
+  return (
+    <footer className="footer">
+      <ul className="footer1">
 
-        <div className="contact">
-          <h2>Contact Us:</h2>
-          <li>
-            <a href="info@dvougue.com">Contact us via email</a>
-          </li>
-          <li>
-            <a href="tel:543-126-8970">Contact us via phone</a>
-          </li>
-        </div>
-      
-        <div className="groupmember">
-          <p>&copy; powered by Iris Ding, Frank Lin, Jiali Liu, Phuong Vu.</p>
-        </div>
-      </footer>
-    );
+        <li className="footeritem">
+          <h2><Link to='/' className="title">Home</Link></h2>
+        </li>
+
+        <li className="footeritem">
+          <h2><Link to='outfitgenerator' className="title">Cloth generator</Link></h2>
+          <ul className="footerul">
+            <li>
+              <Link to='outfitgenerator'>Generate full outfit</Link>
+            </li>
+            <li>
+              <Link to='itemgenerator'>Generate items</Link>
+            </li>
+          </ul>
+        </li>
+
+        <li className="footeritem">
+          <h2><Link to='/quiz' className="title">Style Quiz</Link></h2>
+        </li>
+
+        <li className="footeritem">
+          <h2><Link to='/about' className="title">About</Link></h2>
+        </li>
+
+        <li className="footeritem">
+          <h2 className="title">Social</h2>
+
+          <ul className="footerul">
+            <li>
+              <a href="https://www.instagram.com/">Our instagram</a>
+            </li>
+
+            <li>
+              <a href="https://www.facebook.com/">Our Facebook</a>
+            </li>
+
+            <li>
+              <a href="https://twitter.com/?lang=en">Our twitter</a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+
+      <div className="contact">
+        <h2>Contact Us:</h2>
+        <li>
+          <a href="info@dvougue.com">Contact us via email</a>
+        </li>
+        <li>
+          <a href="tel:543-126-8970">Contact us via phone</a>
+        </li>
+      </div>
+
+      <div className="groupmember">
+        <p>&copy; powered by Iris Ding, Frank Lin, Jiali Liu, Phuong Vu.</p>
+      </div>
+    </footer>
+  );
 }
--- src/MyCloest.js
+++ src/MyCloest.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
 export function Mycloest(props) {
+    const currentUser = props.currentUser
     return(
         <div>
+            <p>welcome {currentUser.userName}</p>
         </div>
 
     );
